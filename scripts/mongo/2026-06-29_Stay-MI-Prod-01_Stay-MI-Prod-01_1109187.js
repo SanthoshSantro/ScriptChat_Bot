24b5a34610b3d8321608a25808ec5be5608a7c81
Santhosh <133766439+SanthoshSantro@users.noreply.github.com>
2026-06-29 10:03:46 +0530

    feat(scripts): add MONGO script for ticket 1109187 by Santhosh.Manivel@agilysys.com

---
+++ scripts/mongo/2026-06-29_Stay-MI-Prod-01_Stay-MI-Prod-01_1109187.js
@@ -0,0 +1,13 @@
+// ═══════════════════════════════════════════════════
+// Script:  2026-06-29_Stay-MI-Prod-01_Stay-MI-Prod-01_1109187.js
+// Ticket:  1109187
+// Author:  Santhosh.Manivel@agilysys.com
+// Project: Marriott-Production
+// Cluster: Stay-MI-Prod-01
+// Database:Stay-MI-Prod-01
+// Shell:   Latest
+// Reason:  final demo
+// Date:    2026-06-29
+// ═══════════════════════════════════════════════════
+
+final demo
